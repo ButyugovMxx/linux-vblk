722e9ccb6df6b1b1feda7094f8878771584a2491
Maxim Butyugov <dmitrijbutugov85436@gmail.com>
2026-06-14 03:36:07 +0300

    fix code style and add build CI

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <linux/string.h>
 #include "stats_vblk.h"
 
-struct vblk_dev{
+struct vblk_dev {
     int major;
     struct gendisk *disk;
     struct file *back_file;
@@ -50,14 +50,18 @@ static void vblk_print_stats(void)
 		vblk.vblk_stats.errors);
 }
 
-static void vblk_submit_bio(struct bio *bio){
+static void vblk_submit_bio(struct bio *bio)
+{
     struct bio *clone;
     u64 sectors;
 
     sectors = bio_sectors(bio);
 
-    if(bio_op(bio) == REQ_OP_READ) vblk_stats_read(&vblk.vblk_stats, sectors);
-    if(bio_op(bio) == REQ_OP_WRITE) vblk_stats_write(&vblk.vblk_stats, sectors);
+    if (bio_op(bio) == REQ_OP_READ)
+	    vblk_stats_read(&vblk.vblk_stats, sectors);
+
+    if (bio_op(bio) == REQ_OP_WRITE)
+	    vblk_stats_write(&vblk.vblk_stats, sectors);
 
     clone = bio_alloc_clone(vblk.back_disk, bio, GFP_NOIO, &vblk.bio_pool);
     if(!clone){
@@ -67,31 +71,34 @@ static void vblk_submit_bio(struct bio *bio){
     }
 
     clone->bi_private = bio;
-    clone -> bi_end_io = vblk_end_io;
-    //pr_info("submit_bio called\n");
+    clone->bi_end_io = vblk_end_io;
 
     submit_bio_noacct(clone);
 }
 
-static void vblk_end_io(struct bio* clone){
+static void vblk_end_io(struct bio* clone)
+{
     struct bio *orig = clone->bi_private;
 
     if(clone->bi_status){ 
         vblk_stats_error(&vblk.vblk_stats);
         bio_io_error(orig);
+    } else {
+        bio_endio(orig);
     }
-    else{bio_endio(orig);}
-    //pr_info("end_io called\n");
+
     bio_put(clone);
 }
 
-static int vblk_open_backend(const char * path){
+static int vblk_open_backend(const char* path)
+{
 	blk_mode_t mode;
 	int err;
+
 	mode = BLK_OPEN_READ | BLK_OPEN_WRITE;
 
 	vblk.back_file = bdev_file_open_by_path(path, mode, &vblk, NULL);
-	if(IS_ERR(vblk.back_file)){
+	if(IS_ERR(vblk.back_file)) {
 		err = PTR_ERR(vblk.back_file);
 		vblk.back_file = NULL;
 		pr_err("failed to open backend: %d\n", err);
@@ -103,7 +110,7 @@ static int vblk_open_backend(const char * path){
 	return 0;
 }	
 
-static void vblk_close_backend(void){
+static void vblk_close_backend(void) {
 	if(vblk.back_file){
 		bdev_fput(vblk.back_file);
 		vblk.back_file = NULL;
@@ -112,18 +119,19 @@ static void vblk_close_backend(void){
 }
 
 
-static int create_device(const char* path){
-
+static int create_device(const char* path)
+{
     struct queue_limits lim = { };
 
     int err = vblk_open_backend(path);
-    if(err) return err;
+    if (err) 
+        return err;
 
     int size_of_sectors = get_capacity(vblk.back_disk->bd_disk);
     vblk.capacity = size_of_sectors;
 
     vblk.disk = blk_alloc_disk(&lim, NUMA_NO_NODE);
-    if(IS_ERR(vblk.disk)){
+    if (IS_ERR(vblk.disk)) {
         err = PTR_ERR(vblk.disk);
         vblk.disk = NULL;
         goto err_back;
@@ -140,7 +148,7 @@ static int create_device(const char* path){
     vblk_stats_init(&vblk.vblk_stats);
 
     err = add_disk(vblk.disk);
-    if(err) goto err_disk;
+    if (err) goto err_disk;
 
     pr_info("mapped over %s\n", path);
     return 0;
@@ -153,8 +161,9 @@ static int create_device(const char* path){
     return err;
 }
 
-static void vblk_destroy_device(void){
-    if(vblk.disk){
+static void vblk_destroy_device(void)
+{
+    if (vblk.disk) {
         del_gendisk(vblk.disk);
         put_disk(vblk.disk);
         vblk.disk = NULL;
@@ -175,7 +184,8 @@ static int vblk_map_dev(const char *arg, const struct kernel_param *ker_par)
     if (vblk.disk)
         return -EBUSY;
 
-    if (!arg) return -EINVAL;
+    if (!arg) 
+        return -EINVAL;
 
     buf = kstrdup(arg, GFP_KERNEL);
     if (!buf)
@@ -199,23 +209,26 @@ static int vblk_map_dev(const char *arg, const struct kernel_param *ker_par)
     return err;
 }
 
-static int vblk_unmap_dev(const char* arg, const struct kernel_param* ker_par){
-    if(!vblk.disk) return -ENODEV;
+static int vblk_unmap_dev(const char* arg, const struct kernel_param* ker_par)
+{
+    if(!vblk.disk) 
+        return -ENODEV;
 
     vblk_destroy_device();
     return 0;
 }
 
-static int __init vblk_init(void){
+static int __init vblk_init(void)
+{
 
     vblk.major = register_blkdev(0,"vblk");
-    if(vblk.major < 0){
+    if (vblk.major < 0) {
         pr_err("failid to register blvk: %d\n", vblk.major);
         return vblk.major;
     }
     
     int err = bioset_init(&vblk.bio_pool, 64, 0, BIOSET_NEED_BVECS);
-    if(err){
+    if (err) {
         unregister_blkdev(vblk.major, "vblk");
         return err;
     }
@@ -224,14 +237,15 @@ static int __init vblk_init(void){
     return 0;
 }
 
-static void __exit vblk_exit(void){
-    if(vblk.disk){
+static void __exit vblk_exit(void)
+{
+    if (vblk.disk) {
         vblk_destroy_device();
     }
 
     bioset_exit(&vblk.bio_pool);
 
-    if(vblk.major > 0){
+    if (vblk.major > 0) {
         unregister_blkdev(vblk.major, "vblk");
     }
     
--- stats_vblk.c
+++ stats_vblk.c
@@ -1,6 +1,7 @@
 #include "stats_vblk.h"
 
-void vblk_stats_init(struct stats *vblk_stats){
+void vblk_stats_init(struct stats *vblk_stats)
+{
     vblk_stats->read_sectors = 0;
     vblk_stats->reads = 0;
     vblk_stats->write_sectors = 0;
@@ -8,18 +9,21 @@ void vblk_stats_init(struct stats *vblk_stats){
     vblk_stats->errors = 0;
 }
 
-void vblk_stats_read(struct stats *vblk_stats, u64 sectors){
+void vblk_stats_read(struct stats *vblk_stats, u64 sectors)
+{
     vblk_stats->reads++;
-    vblk_stats->read_sectors = sectors;
+    vblk_stats->read_sectors += sectors;
 }
 
 
-void vblk_stats_write(struct stats *vblk_stats, u64 sectors){
+void vblk_stats_write(struct stats *vblk_stats, u64 sectors)
+{
     vblk_stats->writes++;
-    vblk_stats->write_sectors = sectors;
+    vblk_stats->write_sectors += sectors;
 }
 
-void vblk_stats_error(struct stats *vblk_stats){
+void vblk_stats_error(struct stats *vblk_stats)
+{
     vblk_stats->errors++;
 }
 
